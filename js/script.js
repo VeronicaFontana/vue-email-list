5e82234084c6255f6fe5aa6fbbcb156802dd67f5
VeronicaFontana <fontana.veronica98@gmail.com>
2023-10-06 16:44:04 +0200

    [ADD] Get one email address

--- js/script.js
+++ js/script.js
@@ -4,8 +4,21 @@ createApp({
 
   data(){
     return{
-      
+      chiamataAxios: "https://flynn.boolean.careers/exercises/api/random/mail",
+      risultatoChiamata: ""
     }
+  },
+  methods:{
+    getApi(){
+      axios.get(this.chiamataAxios)
+      .then((response)=>{
+        this.risultatoChiamata = "Indirizzo email: " + response.data.response
+      })
+    }
+    
+  },
+  mounted(){
+    this.getApi();
   }
 
 }).mount("#app");
